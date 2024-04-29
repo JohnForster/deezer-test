75896f83ac9a8324c6ec3d515b78f6ceb73be91a
John Forster <john.forster@makers.tech>
2024-04-29 15:57:33 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const response = fetch("https://api.deezer.com/genre");
+
+const str = await response.text();
+
+console.log("response.headers:", JSON.stringify(response.headers, null, 2));
+console.log("response.status:", response.status);
+console.log(str);
